8b9e37a891665aa02bef32d3faef20750bddf708
MarianySoares <116230558+MarianySoares@users.noreply.github.com>
2022-11-30 19:32:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function verde(){
+    document.getElementById('verde').style.color ='green'
+}
+function amarelo(){
+    document.getElementById('amarelo').style.color ='yellow'
+}
+function vermelho(){
+    document.getElementById('vermelho').style.color ='green'
+}
